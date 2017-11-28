b704ce9a289639a6d3b29e072edd77508a29e669
sumitkau <sumit.kau@gmail.com>
2017-11-28 21:01:47 +0530

    skillset

--- skillset/main/urls.py
+++ skillset/main/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.home, name='home'),
+#    url(r'', views.home, name='home'),
+    url(r'^$', views.HomePageView.as_view(), name='home'), # Notice the URL has been named
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    url(r'^data/$', views.DataPageView.as_view(), name='data'),  # Add this URL pattern
 ]
--- skillset/main/views.py
+++ skillset/main/views.py
@@ -3,10 +3,63 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView # Import TemplateView
 
 # Create your views here.
 #def home(request):
 #    return HttpResponse("Hello World!")
 
-def home(request):
-    return render(request, "main/home.html", {'message': 'hi, sumit'})
+#def home(request):
+#    return render(request, "main/home.html", {'message': 'hi, sumit'})
+
+# Add the two views we have been talking about  all this time :)
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+# Add this view
+class DataPageView(TemplateView):
+    def get(self, request, **kwargs):
+        # we will pass this context object into the
+        # template so that we can access the data
+        # list in the template
+        context = {
+            'data': [
+                {
+                    'name': 'Celeb 1',
+                    'worth': '3567892'
+                },
+                {
+                    'name': 'Celeb 2',
+                    'worth': '23000000'
+                },
+                {
+                    'name': 'Celeb 3',
+                    'worth': '1000007'
+                },
+                {
+                    'name': 'Celeb 4',
+                    'worth': '456789'
+                },
+                {
+                    'name': 'Celeb 5',
+                    'worth': '7890000'
+                },
+                {
+                    'name': 'Celeb 6',
+                    'worth': '12000456'
+                },
+                {
+                    'name': 'Celeb 7',
+                    'worth': '896000'
+                },
+                {
+                    'name': 'Celeb 8',
+                    'worth': '670000'
+                }
+            ]
+        }
+
+        return render(request, 'data.html', context)
